5f1cb906b70c0b65e84a0e4ccf4cd92347e1b33d
Bhaskarrd10 <bhaskarrd10@gmail.com>
2022-05-18 23:26:42 +0530

    adding image

--- main.py
+++ main.py
@@ -19,11 +19,12 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas= Canvas(width=210, height=240)
+canvas= Canvas(width=210, height=240, bg=YELLOW, highlightthickness=0)
 tomoto_img = PhotoImage(file="tomato.png")
 canvas.create_image(104,112,image=tomoto_img)
+canvas.create_text(104, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 
