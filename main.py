5407be12f09b2bf4ad6969e2b217c59848564813
Bhaskarrd10 <bhaskarrd10@gmail.com>
2022-05-18 23:37:24 +0530

    adding Text

--- main.py
+++ main.py
@@ -21,11 +21,17 @@ window = Tk()
 window.title("pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW ,font= (FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
 canvas= Canvas(width=210, height=240, bg=YELLOW, highlightthickness=0)
 tomoto_img = PhotoImage(file="tomato.png")
 canvas.create_image(104,112,image=tomoto_img)
 canvas.create_text(104, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+
+
 
 
 
